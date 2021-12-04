497e71facceb91c34e6453175d52c7d435c60f3b
Angie <89587005+angie696@users.noreply.github.com>
2021-12-04 03:45:42 +0200

    Create congratulate script Make txt to practice

---
+++ hw_8_big_task/colleague_birthday.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+
+WEEKDAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+weekdays_massive = [[], [], [], [], [], [], []]
+
+
+def find_birthday_boy(text_file):
+    today = datetime.today()
+    start_date = today - timedelta(days=today.weekday())
+    all_weekdays = [(start_date + timedelta(days=idx)).strftime("%d.%m") for idx in range(-2, 5)]
+    days_important = [all_weekdays[:2]] + [all_weekdays[2:]]
+    with open(text_file, 'r') as all_users:
+        for user in all_users:
+            name_and_birth = user.split(":")
+            name = name_and_birth[0]
+            birth = name_and_birth[1].split('.')
+            birthday = datetime(year=today.year, month=int(birth[1]), day=int(birth[0]))
+            birthday = birthday.strftime("%d.%m")
+            for day in range(len(days_important)):
+                if birthday in days_important[day]:
+                    weekdays_massive[day].append(name)
+    return weekdays_massive
+
+
+def get_birthdays_per_week():
+    birthday_boy = find_birthday_boy('scientist_birthday')
+    for idx in range(len(birthday_boy)):
+        print(f"{WEEKDAYS[idx]:<7}: {', '.join(birthday_boy[idx])}")
+
+
+if __name__ == "__main__":
+    get_birthdays_per_week()
